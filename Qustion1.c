5d648e16fdd33a5955f9ce9c3799971081995cd6
Rohan Mondal <91586679+Rohan0617e@users.noreply.github.com>
2022-11-27 02:01:28 +0530

    Update Qustion1.c

--- Qustion1.c
+++ Qustion1.c
@@ -11,7 +11,7 @@ int main()
 {
 	
     char s[100];
-    scanf("[^\n]%*c", s);
+    scanf("%[^\n]%*c", s);
    printf("Hello, World!\n%s",s);
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
